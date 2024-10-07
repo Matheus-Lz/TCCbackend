f8b52f679be454217eb838b4c13ed6a0087fa915
Matheus-Lz <matheus.privado.pb@gmail.com>
2024-10-07 20:50:35 -0300

    Criando Login

--- src/main/java/com/pap/demo/repositories/UserRepository.java
+++ src/main/java/com/pap/demo/repositories/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
    Optional<User> findByCpf(final String cpf);
+   Optional<User> findByEmail(final String email);
 }
---
+++ src/main/java/com/pap/demo/services/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.pap.demo.services;
+
+import com.pap.demo.model.User;
+import com.pap.demo.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com email: " + email));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("USER")  // Defina o papel do usuário (pode ser mais complexo se você tiver roles)
+                .build();
+    }
+}
